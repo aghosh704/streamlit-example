421988eb3d40b049d85400688eb33b60eca05ca6
Aniruddha Ghosh <ozone_lip_0y@icloud.com>
2024-02-11 09:57:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-
+country = st.text_input('Country?')
 path = 'https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-raw-index.csv'
 bigmac_df = pd.read_csv(path)
-bigmac_df_filtered = bigmac_df.loc[bigmac_df['name'] == 'Canada'][['date', 'name', 'dollar_price']]
+bigmac_df_filtered = bigmac_df.loc[bigmac_df['name'] == country][['date', 'name', 'dollar_price']]
 st.line_chart(data=bigmac_df_filtered, x='date', y='dollar_price')
